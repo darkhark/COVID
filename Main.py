8532814878104a110354f0dfdd5ed37ff2314ce7
darkhark <joshua.harkness32@gmail.com>
2020-04-05 12:06:54 -0400

    Removed excess printing

--- Main.py
+++ Main.py
@@ -1,8 +1,6 @@
 from DataCleanser import runDataCleanser
 from DataLoader import runDataLoader
-import pandas as pd
 
-pd.set_option('display.max_columns', None)
 covidDF = runDataLoader()
 print("\n---------Abstract after initial load-------------\n")
 print(covidDF["abstract"])
