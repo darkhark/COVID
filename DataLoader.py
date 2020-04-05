2fc1114cfc3323055ea1c8b90732bb8ef60c24db
Justin Kennedy <justin.kennedy82@gmail.com>
2020-04-05 15:50:00 -0400

    Changed upper to lower case in string

--- DataLoader.py
+++ DataLoader.py
@@ -16,7 +16,7 @@ class FileReader:
                     self.abstract.append(entry['text'])
                 # Body text
             else:
-                content['abstract'] = 'No abstract provided'
+                content['abstract'] = 'no abstract provided'
             for entry in content['body_text']:
                 self.body_text.append(entry['text'])
             self.abstract = '\n'.join(self.abstract)
