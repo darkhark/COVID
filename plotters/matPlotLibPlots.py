9ca568155ca0a3b1942fab2a5c85565307b181ce
Justin Kennedy <justin.kennedy82@gmail.com>
2020-04-23 23:49:49 -0400

    added a hard coded color palette for plotting.

--- plotters/matPlotLibPlots.py
+++ plotters/matPlotLibPlots.py
@@ -52,12 +52,16 @@ def plotWithClusters(X, y_pred, k):
     sns.set(rc={'figure.figsize': (15, 15)})
 
     # colors
-    palette = sns.hls_palette(k, l=.4, s=.9)
+    colors=["#FF0000", "#FF8000", "#FFFF00", "#4C9900", "#00CCCC", "#003366",
+            "#0000FF", "#6600CC", "#FF00FF", "#FF99CC", "#606060", "#000000",
+            "#99FFFF", "#66FF66", "#CC99FF", "#00CC66"]
+    customPalette = sns.set_palette(sns.color_palette(colors))
+    palette = sns.color_palette(palette=customPalette)
 
     # plot
     sns.scatterplot(X[:, 0], X[:, 1], hue=y_pred, legend='full', palette=palette)
     title = "t-SNE with Kmeans " + str(k) + " Labels"
     plt.title(title)
-    location = "plot_pictures/ClusteredTSNE" + str(k) + ".png"
+    location = "plot_pictures/ClusteredTSNE_Justin" + str(k) + ".png"
     plt.savefig(location)
     plt.show()
